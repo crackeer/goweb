8838b41b063ebe7be3a3f277cd3a0941fce482bb
liuhu016 <liuhu016@ke.com>
2022-01-22 14:12:51 +0800

    update

--- container/database.go
+++ container/database.go
@@ -1,21 +1,16 @@
 package container
 
 import (
-	"database/sql"
-	"sync"
-
 	_ "github.com/mattn/go-sqlite3"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var (
-	dbLocker *sync.Mutex
-	DB       *gorm.DB
+	DB *gorm.DB
 )
 
 func InitDB() {
-	dbLocker = &sync.Mutex{}
 	db, err := gorm.Open(sqlite.Open(config.Sqlite3DatabaseFile), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
@@ -25,17 +20,4 @@ func InitDB() {
 
 func GetDatabase() *gorm.DB {
 	return DB
-}
-
-// LockDatabase
-//  @return *sql.DB
-//  @return error
-func LockDatabase() (*sql.DB, error) {
-	dbLocker.Lock()
-	return sql.Open("sqlite3", config.Sqlite3DatabaseFile)
-}
-
-// UnlockDatabase
-func UnlockDatabase() {
-	dbLocker.Unlock()
 }
